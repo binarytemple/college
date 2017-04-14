b70cc50ffdf13998deed4e0d1ca2291d58666b83
bryan hunt <github@binarytemple.co.uk>
2017-04-14 15:33:25 +0100

    fixed user model test

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,11 +3,16 @@ defmodule College.UserTest do
 
   alias College.User
 
-  @valid_attrs %{age: 42, forname: "some content", is_student: true, location: "some content", photo: "some content", salutation: "some content", surname: "some content"}
+  @valid_attrs %{ dob: "1999-01-01", forname: "some content", is_student: true, location: "some content", photo: "some content", salutation: "some content", surname: "some content", email: UUID.uuid1 <> "@foo.com"}
+
+  @valid_attrs_as_user_struct Ecto.Changeset.cast(%User{}, Map.update(@valid_attrs,:dob,nil, &(elem(Ecto.Date.cast(&1 ),1))),  Map.keys(@valid_attrs))
+
+
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @valid_attrs)
+    IO.inspect(changeset)
     assert changeset.valid?
   end
 
