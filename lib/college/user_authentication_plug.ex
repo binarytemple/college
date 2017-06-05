d15d4e17dc72a65202ea386dd258da3fe2af215a
bryan hunt <github@binarytemple.co.uk>
2017-06-05 21:35:21 +0100

    auth flow working but only one type of auth + need to disable fb + google

--- lib/college/user_authentication_plug.ex
+++ lib/college/user_authentication_plug.ex
@@ -1,13 +1,14 @@
 defmodule College.UserAuthentication do
-  import Plug.Conn
+ 
+  alias Phoenix.Controller
+  alias Plug.Conn
 
   def init(opts), do: opts
 
   def authenticated?(conn) do
     # Implement authentication logic here, e.g. check auth headers,
     # session creds, etc.
-    conn = fetch_session(conn)
-    case get_session(conn, :current_user) do
+    case Conn.get_session(conn, :current_user) do
       nil -> false
       _ -> true
     end
@@ -15,12 +16,16 @@ defmodule College.UserAuthentication do
   end
 
   def call(conn, _opts) do
+    conn = conn |> Conn.fetch_session |> Controller.fetch_flash
     if authenticated?(conn) do
       conn
     else
-      conn
-      |> send_resp(401, "")
-      |> halt
+      conn = conn
+      #|> send_resp(401, "")
+      #|> halt
+      |> Controller.put_flash(:error, "unauthenticated")
+      |> Controller.redirect(to: "/admin")
+      |> Conn.halt
     end
   end
 end
